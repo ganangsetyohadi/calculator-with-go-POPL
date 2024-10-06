fb321fbf3247185612a976d198666b579dbd41d9
tarisrizki <m.taris@mhs.usk.ac.id>
2024-10-06 16:16:58 +0700

    menambahkan fitur swap dark mode dan light mode

--- static/script.js
+++ static/script.js
@@ -27,3 +27,16 @@ function toggleSecondInput() {
         secondNumberGroup.style.display = "block";
     }
 }
+
+function toggleTheme() {
+    const body = document.body;
+    const container = document.querySelector('.container');
+    const elementsToToggle = document.querySelectorAll('h1, h2, input, select, button');
+
+    body.classList.toggle('dark-mode');
+    container.classList.toggle('dark-mode');
+
+    elementsToToggle.forEach(el => {
+        el.classList.toggle('dark-mode');
+    });
+}
